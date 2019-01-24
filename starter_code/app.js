ca18346b299690cb1bcac6df8e9100c702155da7
bjornkai <bjornkairoland@gmail.com>
2019-01-23 20:02:55 -0500

    almost done

--- starter_code/app.js
+++ starter_code/app.js
@@ -12,10 +12,51 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
+// in order to use PARTIALS we have to REGISTER them
+hbs.registerPartials(__dirname + "/views/partials");
+
+
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 
+app.get('/beers', (req, res, next) => { 
+// .getBeers => method we get from punkAPI
+  punkAPI.getBeers()
+  // responseFromDB is just placeholder, can be any word
+  .then(responseFromDB => { // =================================> ".this()" holds success callback 
+    // console.log("Response is:", responseFromDB );
 
-app.listen(3000);
+                        // we are renaming responseFromDB to allBeers variable 
+                        // which we'll use in the views
+    res.render('beers-info', { allBeers:responseFromDB })
+  })
+  .catch(error => { // ============================================> ".catch()" is failure callback
+    console.log(error)
+  })
+})
+
+
+
+
+app.get('/random-beer', (req, res, next) => { 
+  // .getRandom()  is method given by punkAPI
+  punkAPI.getRandom()
+  .then(someResponse => {
+    console.log(" = == = == = == == > ", someResponse[0]);
+    res.render('random-beer-info', { randomBeer:someResponse[0] })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+
+
+
+
+
+
+app.listen(3000, () => console.log("no one is listening"));
